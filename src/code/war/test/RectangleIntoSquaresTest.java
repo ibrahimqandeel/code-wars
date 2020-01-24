6d7a9b17c6a10df6d727b2c05e828a9a7fa7abc0
ibrahim <ibrahim@ibrahim-ThinkPad-E580>
2020-01-24 16:09:09 +0300

    Solving Rectangle into Squares problem
    https://www.codewars.com/kata/55466989aeecab5aac00003e/java

---
+++ src/code/war/test/RectangleIntoSquaresTest.java
@@ -0,0 +1,25 @@
+package code.war.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import code.war.kata.RectangleIntoSquares;
+
+public class RectangleIntoSquaresTest {
+	@Test
+	public void test1() {
+		List<Integer> res = new ArrayList<Integer>(Arrays.asList(3, 2, 1, 1));
+		for (int r : res)
+			assertEquals(res, RectangleIntoSquares.cleverSqInRect(5, 3));
+	}
+
+	@Test
+	public void test2() {
+		assertEquals(null, RectangleIntoSquares.cleverSqInRect(5, 5));
+	}
+}
