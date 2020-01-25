ee2a6bbda02b893868a036a46777f1dc39b43da9
ibrahim <ibrahim@ibrahim-ThinkPad-E580>
2020-01-25 18:01:11 +0300

    Practice Merge Sort Again

--- src/code/war/sort/MergeSort.java
+++ src/code/war/sort/MergeSort.java
@@ -3,44 +3,46 @@ package code.war.sort;
 public class MergeSort {
 
 	public static void mergeSort(int[] dataArray) {
-		int arrayLength = dataArray.length;
-		if (arrayLength < 2) {
+		int dataArrayLength = dataArray.length;
+		if (dataArrayLength == 1) {
 			return;
 		}
-		int middle = arrayLength / 2;
+
+		int middle = dataArrayLength / 2;
 		int[] left = new int[middle];
-		int[] right = new int[arrayLength - middle];
+		int[] right = new int[dataArrayLength - middle];
+
 		for (int i = 0; i < left.length; i++) {
 			left[i] = dataArray[i];
 		}
+
 		for (int i = 0; i < right.length; i++) {
 			right[i] = dataArray[middle + i];
 		}
 
 		mergeSort(left);
 		mergeSort(right);
-
 		merge(dataArray, left, right);
 	}
 
 	public static void merge(int[] dataArray, int[] left, int[] right) {
-		int d = 0;
-		int l = 0;
-		int r = 0;
+		int d = 0, l = 0, r = 0;
 		while (l < left.length && r < right.length) {
+			// Ascending sort
 			if (left[l] <= right[r]) {
 				dataArray[d++] = left[l++];
 			} else {
 				dataArray[d++] = right[r++];
 			}
 		}
+
 		while (l < left.length) {
 			dataArray[d++] = left[l++];
 		}
+
 		while (r < right.length) {
 			dataArray[d++] = right[r++];
 		}
-
 	}
 
 	public static void main(String args[]) {
