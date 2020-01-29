46fa441e7c0de0c326f28e878954a963bfb5b314
ibrahim <ibrahim@ibrahim-ThinkPad-E580>
2020-01-29 22:22:21 +0300

    - Solving Equal Sides Of Array Kata
     - Generate test cases

--- src/code/war/kata/EqualSidesOfArray.java
+++ src/code/war/kata/EqualSidesOfArray.java
@@ -1,35 +1,34 @@
 package code.war.kata;
 
+/*
+ * @link https://www.codewars.com/kata/5679aa472b8f57fb8c000047
+ */
 public class EqualSidesOfArray {
 
 	public static int findEvenIndex(int[] arr) {
-		int index = -1;
+		if (arr.length < 2) {
+			return 0;
+		}
 		int rightSum = 0;
 		int leftSum = 0;
-		if (arr.length < 2) {
-			index = 0;
-		}
-		for (int i = 1; i < arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			rightSum += arr[i];
 		}
 
-		for (int i = 1; i < arr.length; i++) {
-			if (leftSum == rightSum) {
-				index = i - 1;
-				break;
-			}
+		for (int i = 0; i < arr.length; i++) {
 			rightSum -= arr[i];
-			leftSum += arr[i - 1];
+
+			if (leftSum == rightSum) {
+				return i;
+			}
+			leftSum += arr[i];
 		}
 
-		return index;
+		return -1;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[] { 1, 10, -100 };
-		System.out.println(findEvenIndex(arr));
-
 	}
 
 }
