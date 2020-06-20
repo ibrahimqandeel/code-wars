0eb4ac1ca7b0a01e3b65cde427d129ab293aa381
Ibrahim Qandeel <ibrahim.s.qandeel@gmail.com>
2020-06-20 21:53:54 +0300

    - LeetCode Challenges

--- src/code/war/test/BubbleSortTest.java
+++ src/code/war/test/BubbleSortTest.java
@@ -1,20 +1,19 @@
 package code.war.test;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import org.junit.jupiter.api.Test;
-
 import code.war.algorithms.sort.BubbleSort;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 class BubbleSortTest {
 
-	@Test
-	void sortMethodTest() {
-		assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, BubbleSort.sort(new int[] { 5, 4, 3, 2, 1 }));
+    @Test
+    void sortMethodTest() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, BubbleSort.sort(new int[]{5, 4, 3, 2, 1}));
 
-		assertArrayEquals(new int[] { 1, 2, 4, 6, 8, 10, 33, 55, 80, 81 },
-				BubbleSort.sort(new int[] { 55, 10, 8, 6, 80, 2, 81, 1, 4, 33 }));
+        assertArrayEquals(new int[]{1, 2, 4, 6, 8, 10, 33, 55, 80, 81},
+                BubbleSort.sort(new int[]{55, 10, 8, 6, 80, 2, 81, 1, 4, 33}));
 
-		assertArrayEquals(new int[] { 1, 2, 3 }, BubbleSort.sort(new int[] { 1, 2, 3 }));
-	}
+        assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sort(new int[]{1, 2, 3}));
+    }
 }
